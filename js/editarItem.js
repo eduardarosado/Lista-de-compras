70cc5b46ce26dba46c031a039d26ecb1b799a226
Maria Eduarda Rosado <95380088+eduardarosado@users.noreply.github.com>
2024-11-16 15:10:39 -0300

    Criada a lista de compras

---
+++ js/editarItem.js
@@ -0,0 +1,18 @@
+export const editarItem = (elemento) => {
+    let novoItem = prompt("Digite o novo nome do item:");
+
+
+    if(novoItem !== null && novoItem.trim() !== ""){
+        const itemTextoAtualizado = elemento.querySelector("#item-titulo");
+        itemTextoAtualizado.textContent = novoItem;
+      
+
+        const estavaComprado = elemento.querySelector(".input-checkbox").checked;
+      if(estavaComprado){
+        elemento.querySelector("input-checkbox").checked = true;
+        elemento.querySelector(".checkbox-item-customizado").classList.add("checked");
+        itemTextoAtualizado.style.textDecoration = "line-througt";
+      }  
+
+    }
+  }
